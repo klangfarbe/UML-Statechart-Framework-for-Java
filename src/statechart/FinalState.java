6fd1d96b153e082cf885dc1ffb3d7909813b469b
Christian Mocek <christian.mocek@googlemail.com>
2010-09-03 23:02:07 +0200

    Moved the project code from sourceforge to github and deleted the old C++ implementation which was not maintained for ages. Maybe the C++ version will be completely rewritten.

---
+++ src/statechart/FinalState.java
@@ -0,0 +1,33 @@
+/*
+ * UML statechart framework (http://github.com/klangfarbe/UML-Statechart-Framework-for-Java)
+ *
+ * Copyright (C) 2006-2010 Christian Mocek (christian.mocek@googlemail.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+package statechart;
+
+/**
+ * The final state of a statechart or substate
+ */
+public class FinalState extends State {
+  /**
+   * Creates a new Finalstate.
+   * @throws StatechartException 
+   */
+  public FinalState(Context parent) throws StatechartException {
+    super(parent, null, null, null);
+  };
+}
